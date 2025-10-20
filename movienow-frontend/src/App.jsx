0fabeaade345e6687c0a8f57525da91dc14bbd19
ARRUABARRENA MUÑOZ Juan Bautista <jarruabarrena@correo.um.edu.uy>
2025-10-19 21:36:56 -0300

    cambios en home

--- movienow-frontend/src/App.jsx
+++ movienow-frontend/src/App.jsx
@@ -2,6 +2,7 @@ import Register from "./register/Register";
 import Login from "./login/login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProfileEdit from "./editProfile/editProfile";
+import Home from "./home/Home";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                 <Route path="/signup" element={<Register />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/edit-profile" element={<ProfileEdit />} />
+                <Route path="/home" element={<Home />} />
             </Routes>
         </Router>
     );
--- movienow-frontend/src/home/Home.jsx
+++ movienow-frontend/src/home/Home.jsx
@@ -2,11 +2,37 @@ import "./Home.css";
 import LogoB from "../assets/images/movienow-logo-w-m.png";
 import Header from "../components/Header/Header.jsx";
 import Footer from "../components/Footer/Footer.jsx";
+import { User2 } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect, useRef } from "react";
 
 
 
 export default function Home() {
 
+    const navigate = useNavigate();
+    const [menuOpen, setMenuOpen] = useState(false);
+    const menuRef = useRef(null);
+
+    function toggleMenu() {
+        setMenuOpen((prev) => !prev);
+    }
+
+    function goToProfile() {
+        navigate("/edit-profile");
+        setMenuOpen(false); 
+    }
+
+
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (menuRef.current && !menuRef.current.contains(event.target)) {
+            setMenuOpen(false);
+        }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+    }, []);
 
 
     return (
@@ -15,6 +41,19 @@ export default function Home() {
             <Header className="register-header">
 
                 <img className="logo-header-home" src={LogoB} alt="MovieNow logo"/>
+
+                <div className="user-menu-container" ref={menuRef}>
+                    <User2 className="user-icon" onClick={toggleMenu} />
+
+                    {menuOpen && (
+                        <div className="user-dropdown">
+                        <button onClick={goToProfile}>Editar perfil</button>
+                        <button>Ejemplo</button>
+                        <button>Mis Peliculas</button>
+                        <button>Cerrar sesión</button>
+                        </div>
+                )}
+                </div>
 
 
             </Header>
